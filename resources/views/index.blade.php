15993d82e50ea677fa4e01ab8a6c446fe8384edc
mcmellado <celeste.mellado@iesdonana.org>
2023-11-13 11:47:49 +0100

    configurando modelos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/jpg" href="./media/imagenes/favicon.ico"/>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <title>Bienvenido a ShareFood</title>
 </head>
 <body>
 
-    <header class="bg-light p-3">
+    <header class="bg-custom p-3">
         <nav class="container">
-            <a href="#" class="text-decoration-none me-3">Sobre nosotros</a>
-            <a href="#" class="text-decoration-none">Contacto</a>
+            <a href="#" class="text-decoration-none text-light me-3">Sobre nosotros</a>
+            <a href="#" class="text-decoration-none text-light">Contacto</a>
         </nav>
     </header>
 
@@ -24,11 +25,11 @@
             <div class="col-md-6">
                 <form>
                     <div class="mb-3">
-                        <label for="correo" class="form-label">Correo electrónico o teléfono</label>
+                        <label for="correo" class="form-label">Usuario o correo electrónico:</label>
                         <input type="text" class="form-control" id="correo">
                     </div>
                     <div class="mb-3">
-                        <label for="contrasenia" class="form-label">Contraseña</label>
+                        <label for="contrasenia" class="form-label">Contraseña:</label>
                         <input type="password" class="form-control" id="contrasenia">
                     </div>
                     <div class="mb-3 form-check">
@@ -42,14 +43,12 @@
         </div>
     </div>
 
-    <footer class="bg-light p-4 mt-5 fixed-bottom">
-        <div class="container text-center">
+    <footer class="bg-custom p-4 mt-5 fixed-bottom">
+        <div class="container text-center text-light">
             <p>&copy; 2023 ShareFood. Todos los derechos reservados.</p>
         </div>
     </footer>
     
-    
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('index');
@@ -27,5 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
 
 require __DIR__.'/auth.php';
