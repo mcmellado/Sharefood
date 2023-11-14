8660bc524108b8840ef12e78aa0e3e64932b848d
mcmellado <celeste.mellado@iesdonana.org>
2023-11-14 12:22:46 +0100

    creando registro

---
+++ resources/views/registro.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/jpg" href="./media/imagenes/favicon.ico"/>
+    <link rel="stylesheet" href="{{ asset('css/registro.css') }}">
+    <title>Bienvenido a ShareFood - Registro</title>
+</head>
+<body>
+
+    <header class="header">
+        <nav class="container">
+            <a href="#" class="text-decoration-none text-light me-3">Sobre nosotros</a>
+            <a href="#" class="text-decoration-none text-light">Contacto</a>
+        </nav>
+    </header>
+
+    <div class="container mt-5">
+        <h1 class="display-3 text-center">¡Regístrate en ShareFood!</h1>
+        <p class="lead text-center">Crea tu cuenta y descubre los mejores lugares para disfrutar de deliciosas comidas.</p>
+
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-6">
+                <form method="POST" action="{{ route('validar-registro') }}">
+                    @csrf 
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Nombre de usuario:</label>
+                        <input type="text" class="form-control @error('usuario') is-invalid @enderror" id="usuario" name="usuario" value="{{ old('usuario') }}">
+                        @error('usuario')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo electrónico:</label>
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Contraseña:</label>
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                        @error('password')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="password_confirmation" class="form-label">Confirmar contraseña:</label>
+                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                    </div>
+                    <button type="submit" class="btn btn-dark">Registrarse</button>
+                </form>     
+                <p class="mt-3">¿Ya tienes una cuenta? <a href="{{ route('login') }}">Inicia sesión aquí</a></p>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-custom p-4 mt-5 fixed-bottom">
+        <div class="container text-center text-light">
+            <p>&copy; 2023 ShareFood. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,11 @@
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::view('/registro', 'register')->name('registro');
+Route::view('/registro', 'registro')->name('registro');
 Route::view('/perfil', 'perfil')->name('perfil');
 
-Route::post('/validar-registro', [LoginController::class, 'register'])->name('validar-registro');
+Route::post('/validar-registro', [LoginController::class, 'registro'])->name('validar-registro');
 Route::match(['get', 'post'], '/inicia-sesion', [LoginController::class, 'login'])->name('inicia-sesion');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::match(['get', 'post'], '/inicia-sesion', [LoginController::class, 'login'])->name('inicia-sesion');
