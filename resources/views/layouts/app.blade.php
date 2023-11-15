38a0904e9685790649005bded8f5364c7b16a70f
mcmellado <celeste.mellado@iesdonana.org>
2023-11-15 16:43:46 +0100

    corrigiendo cosas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <!-- ... (código omitido para brevedad) ... -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
@@ -10,7 +9,6 @@
     @include('layouts.navigation')
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        <!-- Page Heading -->
         @if (isset($header))
             <header class="bg-white dark:bg-gray-800 shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -18,8 +16,6 @@
                 </div>
             </header>
         @endif
-
-        <!-- Page Content -->
         <main>
             @yield('content')
         </main>
